1f6c6c8b9a69a405148bd8be75268fb66a8c3962
unknown <pbalduino+github@gmail.com>
2014-11-29 13:00:27 -0200

    A better demo screen, rich UI and empty UI

--- 5 semestre/prointer/PostmonActiveClient/postmon-demo.js
+++ 5 semestre/prointer/PostmonActiveClient/postmon-demo.js
@@ -1,11 +1,11 @@
 window.onload = function() {
 
-  new PostmonClient("http://api.postmon.com.br/v1/cep/")
+  new PostmonClient("http://api.postmon.com.br/v1/cep/", new PostmonRichUI())
     .search("03979000", function(data) {
       console.log(data);
     });
 
-  new PostmonClient("http://api.postmon.com.br/v1/cep/")
+  new PostmonClient("http://api.postmon.com.br/v1/cep/", new PostmonEmptyUI())
     .search("99999999", function(data) {
       console.log(data);
     });
--- 5 semestre/prointer/PostmonActiveClient/postmon.js
+++ 5 semestre/prointer/PostmonActiveClient/postmon.js
@@ -1,55 +1,61 @@
-function PostmonClient(baseURL) {
-  var uniqueId = "postmon-waiter-" + Math.round(Math.random() * 0x7fff);
-
-  var showWait = function() {
-    document.getElementById(uniqueId).style.visibility = "visible";
+function Postmon(options) {
+  var PostmonEmptyUI = function() {
+    this.showWait = function() {};
+    this.hideWait = function() {};
   }
 
-  var hideWait = function() {
-    document.getElementById(uniqueId).style.visibility = "hidden";
-  }
+  var PostmonRichUI = function() {
+    var uniqueId = "postmon-waiter-" + Math.round(Math.random() * 0x7fff);
 
-  var createWaiter = function() {
-    var text = document.createTextNode("Aguarde - pesquisando CEP.");
-
-    var attrClass = document.createAttribute("class");
-    attrClass.value = "postmon-waiter";
-
-    var attrId = document.createAttribute("id");
-    attrId.value = uniqueId;
-
-    var element = document.createElement("div");
-    element.appendChild(text);
-    element.setAttributeNode(attrClass);
-    element.setAttributeNode(attrId);
-
-    document.body.appendChild(element);
-  }
-
-  this.search = function(zip, callback) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function(event) {     
-      if (xhr.readyState === 4 ) {
-        hideWait();
-
-        if(xhr.status === 200) {
-          return callback(JSON.parse(xhr.responseText), xhr, event);
-        } else {
-          return callback({status: xhr.status}, xhr, event);
-        };
-      };
+    this.showWait = function() {
+      document.getElementById(uniqueId).style.visibility = "visible";
     };
 
-    showWait();
-    xhr.open("GET", baseURL + zip, true);
-    xhr.send();
+    this.hideWait = function() {
+      document.getElementById(uniqueId).style.visibility = "hidden";
+    };
+
+    var createWaiter = function() {
+      var text = document.createTextNode("Aguarde - pesquisando CEP.");
+
+      var attrClass = document.createAttribute("class");
+      attrClass.value = "postmon-waiter";
+
+      var attrId = document.createAttribute("id");
+      attrId.value = uniqueId;
+
+      var element = document.createElement("div");
+      element.appendChild(text);
+      element.setAttributeNode(attrClass);
+      element.setAttributeNode(attrId);
+
+      document.body.appendChild(element);
+    };
+
+    createWaiter();
   }
 
-  createWaiter();
-}
+  var PostmonClient = function(baseURL, uiManager) {
+    this.search = function(zip, callback) {
+      var xhr = new XMLHttpRequest();
 
-function Postmon(options) {
+      xhr.onreadystatechange = function(event) {     
+        if (xhr.readyState === 4 ) {
+          uiManager.hideWait();
+
+          if(xhr.status === 200) {
+            return callback(JSON.parse(xhr.responseText), xhr, event);
+          } else {
+            return callback({status: xhr.status}, xhr, event);
+          };
+        };
+      };
+
+      uiManager.showWait();
+      xhr.open("GET", baseURL + zip, true);
+      xhr.send();
+    }
+  }
 
   var LocalStorage = function(id) {
 
@@ -67,15 +73,13 @@ function Postmon(options) {
       key = id + "." + key;
       var value = localStorage[key];
 
-      if(value === undefined) {
-        return undefined;
-      }
+      if(value !== undefined) {
+        var obj = JSON.parse(value);
 
-      var obj = JSON.parse(value);
-
-      return (obj["type"] === "object") ?
-             JSON.parse(obj["value"]) :
-             obj["value"];
+        return (obj["type"] === "object") ?
+               JSON.parse(obj["value"]) :
+               obj["value"];
+      };
     };
 
     this.remove = function(key) {
@@ -84,6 +88,6 @@ function Postmon(options) {
   }
 
   var storage = new LocalStorage("postmon.cache");
-  var client  = new PostmonClient("http://api.postmon.com.br/v1/cep/");
-  var timer   = new PostmonDaemon(5000, storage, client);
+  var client  = new PostmonClient("http://api.postmon.com.br/v1/cep/", new PostmonUI());
+  var daemon  = new PostmonDaemon(5000, storage, client);
 }
