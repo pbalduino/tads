10d8ceab490a42e95d5ad9b42182bd9ed76ef72d
unknown <pbalduino+github@gmail.com>
2014-11-29 11:50:13 -0200

    ajax

--- 5 semestre/prointer/PostmonActiveClient/postmon-demo.js
+++ 5 semestre/prointer/PostmonActiveClient/postmon-demo.js
@@ -1,28 +1,13 @@
-var storage = new LocalStorage();
+window.onload = function() {
 
-if(!storage.read("demo.isRunning")) {
-  alert("Let's write something on the local storage");
+  new PostmonClient("http://api.postmon.com.br/v1/cep/")
+    .search("03979000", function(data) {
+      console.log(data);
+    });
 
-  storage.insert("demo.isRunning", true);
-  storage.insert("demo.text", "It works!");
-  storage.insert("demo.object", {"key": "value", another_key: "bacon"});
-  storage.insert("demo.float", 3.14);
-  storage.insert("demo.integer", 42);
+  new PostmonClient("http://api.postmon.com.br/v1/cep/")
+    .search("99999999", function(data) {
+      console.log(data);
+    });
 
-  window.location.reload();
-
-} else {
-  alert("Value of stored value is '" + storage.read("demo.text") + "'");
-
-  console.log(storage.read("demo.object"),
-              storage.read("demo.isRunning"),
-              storage.read("demo.text"),
-              storage.read("demo.float"),
-              storage.read("demo.integer"));
-
-  storage.remove("demo.object");
-  storage.remove("demo.float");
-  storage.remove("demo.integer");
-  storage.remove("demo.isRunning");
-  storage.remove("demo.text");
-}
+};
--- 5 semestre/prointer/PostmonActiveClient/postmon.js
+++ 5 semestre/prointer/PostmonActiveClient/postmon.js
@@ -1,13 +1,70 @@
+function PostmonClient(baseURL) {
+  var uniqueId = "postmon-waiter-" + Math.round(Math.random() * 0x7fff);
+
+  var showWait = function() {
+    document.getElementById(uniqueId).style.visibility = "visible";
+  }
+
+  var hideWait = function() {
+    document.getElementById(uniqueId).style.visibility = "hidden";
+  }
+
+  var createWaiter = function() {
+    var text = document.createTextNode("Aguarde - pesquisando CEP.");
+
+    var attrClass = document.createAttribute("class");
+    attrClass.value = "postmon-waiter";
+
+    var attrId = document.createAttribute("id");
+    attrId.value = uniqueId;
+
+    var element = document.createElement("div");
+    element.appendChild(text);
+    element.setAttributeNode(attrClass);
+    element.setAttributeNode(attrId);
+
+    document.body.appendChild(element);
+  }
+
+  this.search = function(zip, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(event) {     
+      if (xhr.readyState === 4 ) {
+        hideWait();
+
+        if(xhr.status === 200) {
+          return callback(JSON.parse(xhr.responseText), xhr, event);
+        } else {
+          return callback({status: xhr.status}, xhr, event);
+        };
+      };
+    };
+
+    showWait();
+    xhr.open("GET", baseURL + zip, true);
+    xhr.send();
+  }
+
+  createWaiter();
+}
+
 function Postmon(options) {
 
-  var LocalStorage = function() {
+  var LocalStorage = function(id) {
 
     this.insert = function(key, value) {
       var type = typeof(value);
-      localStorage[key] = JSON.stringify({type: type, value: type === "object" ? JSON.stringify(value) : value});
-    }
+      key = id + "." + key;
+
+      localStorage[key] = JSON.stringify({type:  type, 
+                                          value: type === "object" ?
+                                                 JSON.stringify(value) :
+                                                 value});
+    };
 
     this.read = function(key) {
+      key = id + "." + key;
       var value = localStorage[key];
 
       if(value === undefined) {
@@ -19,13 +76,14 @@ function Postmon(options) {
       return (obj["type"] === "object") ?
              JSON.parse(obj["value"]) :
              obj["value"];
-    }
+    };
 
-    this.remove = localStorage.removeItem;
+    this.remove = function(key) {
+      localStorage.removeItem(id + "." + key);
+    };
   }
 
-  var storage = new LocalStorage();
-  var client  = new PostmonClient();
-
-  var timer   = new Timer(5000, storage, client);
+  var storage = new LocalStorage("postmon.cache");
+  var client  = new PostmonClient("http://api.postmon.com.br/v1/cep/");
+  var timer   = new PostmonDaemon(5000, storage, client);
 }
